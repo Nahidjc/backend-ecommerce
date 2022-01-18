be5e62e2202dd5de4257a9dfc741a5bb70459fb8
Nahidjc <nahidjc80@gmail.com>
2022-01-18 22:06:58 +0600

    api working

--- base/serializers.py
+++ base/serializers.py
@@ -4,7 +4,7 @@ from .models import Product
 from rest_framework import serializers
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
--- base/views.py
+++ base/views.py
@@ -1,11 +1,15 @@
+
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .products import products
-from .models import Product
+from base.products import products
+from base.models import Product
 
+from django.http import HttpResponse, JsonResponse
 from .serializers import ProductSerializer
 # Create your views here.
+
+from rest_framework import generics
 
 
 @api_view(['GET', 'POST'])
@@ -15,10 +19,17 @@ def getRoutes(request):
 
 @api_view(['GET', 'POST'])
 def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-
+    if request.method == 'GET':
+        products = Product.objects.all()
+        print(len(products))
+        serializer = ProductSerializer(products, many=True)
+        print(serializer.data)
     return Response(serializer.data)
+
+
+# class getProducts(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
 
 @api_view(['GET', 'POST'])
