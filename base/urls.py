7666e007d89091c0d9d4c4851c4ed81969aad379
Nahidjc <nahidjc80@gmail.com>
2022-01-29 23:57:52 +0600

    jwt install

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from base import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+
+)
+
+
 urlpatterns = [
+    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('routes', views.getRoutes, name='routes'),
     path('products', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product')
