97359e93fd35064f8e633c0aced07ab065775ed7
Nahidjc <nahidjc80@gmail.com>
2022-01-30 23:42:34 +0600

    user authentication token

--- base/urls.py
+++ base/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('users/login', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('routes', views.getRoutes, name='routes'),
+    path('users/profile/', views.getUserProfile, name='users-profile'),
     path('products', views.getProducts, name='products'),
     path('products/<str:pk>', views.getProduct, name='product')
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -6,7 +5,7 @@ from base.products import products
 from base.models import Product
 
 from django.http import HttpResponse, JsonResponse
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -18,13 +17,22 @@ from rest_framework import generics
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        data["username"] = self.user.username
-        data["email"] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
         return data
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 @api_view(['GET', 'POST'])
