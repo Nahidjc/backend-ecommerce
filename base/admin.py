3ec588190912705179ca2293a53e0b63362cd71e
Nahidjc <nahidjc80@gmail.com>
2022-03-07 17:45:36 +0600

    new database

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Review, Order, OrderItem, ShippingAddress, Category
+from .models import Product, Review, Order, OrderItem, ShippingAddress
 # Register your models here
 
 admin.site.register(Product)
@@ -7,4 +7,3 @@ admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
-admin.site.register(Category)
--- base/models.py
+++ base/models.py
@@ -3,23 +3,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Category(models.Model):
-    name = models.CharField(
-        max_length=100, null=True)
-    image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, default=1, null=True, blank=True)
-    # category = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     rating = models.DecimalField(
         max_digits=7, decimal_places=2, null=True, blank=True)
