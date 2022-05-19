e9ee07aa1678e8214cfb0c321848e9b8c2218c92
Nahid Hasan <nahidjc80@gmail.com>
2022-05-19 21:24:56 +0600

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -164,9 +164,9 @@ MEDIA_ROOT = 'static/images'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'nahidjc.cse@gmail.com'
+
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = 'Nahid2194@#'
+
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 print('From settings', EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
 EMAIL_PORT = 587
